3b45eda9d839a0a09d86b73037c67eafc81d577c
spyhere <gekav3@mail.ru>
2020-04-05 02:53:15 +0400

    commit

---
+++ precache-manifest.e7e39c59ce214c04293f5dfc6c88e773.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b833c80fc82cc3beacfa20773e5bcd72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df4b5a763b121ca670f2",
+    "url": "/static/css/main.f9a31216.chunk.css"
+  },
+  {
+    "revision": "f8c028c984cbbd4f373a",
+    "url": "/static/js/2.c54ace6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c54ace6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4b5a763b121ca670f2",
+    "url": "/static/js/main.1cefb059.chunk.js"
+  },
+  {
+    "revision": "40318e78103fad2db0d0",
+    "url": "/static/js/runtime-main.3b308300.js"
+  },
+  {
+    "revision": "3f662545260dfb088afa435350648aa3",
+    "url": "/static/media/Logo_design_var02_01.3f662545.png"
+  },
+  {
+    "revision": "4c76c8f369a19592b58ff5d7e0b6f105",
+    "url": "/static/media/inf01.4c76c8f3.png"
+  },
+  {
+    "revision": "c568ebdcf4a33cc9ef1ce0f95741253c",
+    "url": "/static/media/inf02.c568ebdc.png"
+  },
+  {
+    "revision": "74511d91cd74565d8596f662e16f10b0",
+    "url": "/static/media/inf03.74511d91.png"
+  }
+]);

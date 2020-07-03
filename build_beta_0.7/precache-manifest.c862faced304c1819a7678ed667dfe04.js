9b6fae84cc46f4838d808ba22a5fc0aca45930c8
spyhere <58670340+spyhere@users.noreply.github.com>
2020-07-03 16:30:52 +0400

    beta build 0.74

---
+++ build_beta_0.7/precache-manifest.c862faced304c1819a7678ed667dfe04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291414bf9d7b2c52bb98084b20ac8ac8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4dfacad850fba993470",
+    "url": "./static/css/main.3eb3e9de.chunk.css"
+  },
+  {
+    "revision": "16a18d0b36ffd8f67905",
+    "url": "./static/js/2.f0a3efdc.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.f0a3efdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dfacad850fba993470",
+    "url": "./static/js/main.1e9ade89.chunk.js"
+  },
+  {
+    "revision": "4e65a1c15e4cc730dcf1",
+    "url": "./static/js/runtime-main.f55990d0.js"
+  },
+  {
+    "revision": "3019756d80dd41612d2544cfa8f23603",
+    "url": "./static/media/clap01.3019756d.mp3"
+  },
+  {
+    "revision": "14d657e73ecbd89cbce4d9c97d3455bb",
+    "url": "./static/media/metro_1.14d657e7.mp3"
+  },
+  {
+    "revision": "29b77406df152d3f0f45ea5c864a8aff",
+    "url": "./static/media/metro_other.29b77406.mp3"
+  }
+]);
--- build_beta_0.7/service-worker.js
+++ build_beta_0.7/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3fe224cf5ed921423d19e18870a4020b.js"
+  "./precache-manifest.c862faced304c1819a7678ed667dfe04.js"
 );
 
 self.addEventListener('message', (event) => {

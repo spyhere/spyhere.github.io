e9e7e8d261bc4448ff7a3a4ce13415763e0e0621
spyhere <58670340+spyhere@users.noreply.github.com>
2020-07-07 21:32:23 +0400

    Beta build 0.79

---
+++ build_beta_0.7/precache-manifest.fe71f18424e638b9e9f8cc0b551378e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320e46214efc13dd8dd888a9ee7cff72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74e54a83807e4548034f",
+    "url": "./static/css/main.994b60fb.chunk.css"
+  },
+  {
+    "revision": "240b02a5f3098dbb49ad",
+    "url": "./static/js/2.b2115d9e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.b2115d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e54a83807e4548034f",
+    "url": "./static/js/main.4c8256c8.chunk.js"
+  },
+  {
+    "revision": "4e65a1c15e4cc730dcf1",
+    "url": "./static/js/runtime-main.f55990d0.js"
+  },
+  {
+    "revision": "3019756d80dd41612d2544cfa8f23603",
+    "url": "./static/media/clap01.3019756d.mp3"
+  },
+  {
+    "revision": "14d657e73ecbd89cbce4d9c97d3455bb",
+    "url": "./static/media/metro_1.14d657e7.mp3"
+  },
+  {
+    "revision": "29b77406df152d3f0f45ea5c864a8aff",
+    "url": "./static/media/metro_other.29b77406.mp3"
+  }
+]);

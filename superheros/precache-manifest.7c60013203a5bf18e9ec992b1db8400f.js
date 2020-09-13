56e57284f375406d60030c8403c4ccd5fa47f23b
spyhere <58670340+spyhere@users.noreply.github.com>
2020-09-13 20:07:26 +0400

    upload

---
+++ superheros/precache-manifest.7c60013203a5bf18e9ec992b1db8400f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0dd36a242716f7fabdd75ee8d3f6cb",
+    "url": "/superheros/index.html"
+  },
+  {
+    "revision": "1235180330dc02361897",
+    "url": "/superheros/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "abb9e33b0e96b40fecd9",
+    "url": "/superheros/static/css/main.61a69bce.chunk.css"
+  },
+  {
+    "revision": "1235180330dc02361897",
+    "url": "/superheros/static/js/2.d8f2d37e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/superheros/static/js/2.d8f2d37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb9e33b0e96b40fecd9",
+    "url": "/superheros/static/js/main.06217ca0.chunk.js"
+  },
+  {
+    "revision": "49140bbd7f4a149ff15f",
+    "url": "/superheros/static/js/runtime-main.1291103d.js"
+  }
+]);

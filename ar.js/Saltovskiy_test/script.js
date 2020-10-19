22b0f42ff1b3d9044a33de93658855a983cf3ae8
spyhere <58670340+spyhere@users.noreply.github.com>
2020-10-19 15:41:56 +0300

    logic update, speaker update

--- ar.js/Saltovskiy_test/script.js
+++ ar.js/Saltovskiy_test/script.js
@@ -1,10 +1,11 @@
-let entity = null, markerVisible = false, cursor, raycaster, text, textInterval = new Array(), iOs = false,
+let entity = null, markerVisible = false, cursor, raycaster, text, textInterval = new Array(), iOs = false, audioOn = false,
 audio01, audio01Timer, widthMax = 0.515, heightMax = 0.23, listenerPress = "mousedown", listenerUp = "mouseup";
 
 AFRAME.registerComponent('markerhandler', {
     init: function () {
       this.el.sceneEl.addEventListener('markerFound', () => {
         markerVisible = true;
+
         if (!textInterval.length) {
           text[0].emit("restart");
           textInterval[0] = setInterval(() => text[0].emit("restart"), 12000)
@@ -29,57 +30,50 @@ AFRAME.registerComponent('markerhandler', {
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 window.onload = () => {
   if (window.innerWidth < 768) {
-    listenerPress = "touchstart"; 
-    listenerUp = "touchend"; 
-
-    widthMax = 0.27;
-    heightMax = 0.39;
+    changeInputNames()
   }
 
   // audio01 = document.querySelector("#makeba");
   audio01 = new Audio();
 
+  // querySelector
   document.querySelector(".arjs-loader").style.display = "none";
-
   text = document.querySelectorAll("a-image");
 
+  // iOs check
   iOs = getMobileOperatingSystem();
 
   if (iOs) {
-    let img = document.querySelector("#micro");
+    let img = document.querySelector("#speaker");
     img.style.display = "block";
-    img.addEventListener("touchstart", () => {
-      img.setAttribute("src", "./micro_on.png");
-      audio01.play();
-      audio01.src = './Jain - Makeba.mp3';
-    })
+    img.addEventListener("touchstart", appleTouch)
   } else {
     audio01.src = './Jain - Makeba.mp3';
   }
 
+  // // Cursos & Raycaster
+  // cursor = document.querySelector("a-cursor");
+  // raycaster = cursor.getAttribute("raycaster");
 
-  cursor = document.querySelector("a-cursor");
-  raycaster = cursor.getAttribute("raycaster");
+  // raycaster.direction = {x: 100, y: 100, z: -1}
 
-  raycaster.direction = {x: 100, y: 100, z: -1}
+  // document.addEventListener(`${listenerPress}`, (el) => {
+  //   // cursor.setAttribute("visible", "true");
+  //   if (window.innerWidth < 768) {
+  //     el.clientX = el.changedTouches[0].clientX;
+  //     el.clientY = el.changedTouches[0].clientY;
+  //   }
 
-  document.addEventListener(`${listenerPress}`, (el) => {
-    // cursor.setAttribute("visible", "true");
-    if (window.innerWidth < 768) {
-      el.clientX = el.changedTouches[0].clientX;
-      el.clientY = el.changedTouches[0].clientY;
-    }
-
-    let [x, y] = convertingCoords(el.clientX, el.clientY);
-    cursor.object3D.position.set(x, y, -1);
-    raycaster.direction = {x: x, y: y, z: -1};
+    // let [x, y] = convertingCoords(el.clientX, el.clientY);
+    // cursor.object3D.position.set(x, y, -1);
+    // raycaster.direction = {x: x, y: y, z: -1};
     
-  })
+  // })
 
-  document.addEventListener(`${listenerUp}`, () => {
-    cursor.setAttribute("visible", "false");
-    raycaster.direction = {x: 100, y: 100, z: -1}
-  })
+  // document.addEventListener(`${listenerUp}`, () => {
+  //   cursor.setAttribute("visible", "false");
+  //   raycaster.direction = {x: 100, y: 100, z: -1}
+  // })
   
 }
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
@@ -101,10 +95,31 @@ function getMobileOperatingSystem() {
     if (/android/i.test(userAgent)) {
         return false;
     }
-
+    // Apple production is detected
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         return true;
     }
 
     return false;
+}
+
+function changeInputNames() {
+  listenerPress = "touchstart"; 
+  listenerUp = "touchend"; 
+
+  widthMax = 0.27;
+  heightMax = 0.39;
+}
+
+function appleTouch() {
+  if (!audioOn) {
+    img.setAttribute("src", "./sound_on.png");
+    audio01.play();
+    audioOn = true;
+  } else {
+    img.setAttribute("src", "./sound_off.png");
+    audio01.pause();
+    audioOn = false;
+  }
+  if (!audio01.src.length) audio01.src = './Jain - Makeba.mp3'; 
 }
